b23103c78043a19fc05a21c41df15143bf6905dd
Peter Nelson <afrusnoped@hotmail.com>
2022-04-28 18:24:30 -0500

    feat: delete selected rows from grid by using backspace or delete keys while rows are selected

--- src/pages/student-grid/student-grid.js
+++ src/pages/student-grid/student-grid.js
@@ -7,23 +7,39 @@ import 'ag-grid-community/dist/styles/ag-theme-alpine.css'
 export const StudentGrid = () => {
 
   const [rowData] = useState([
-    {firstName: 'Peter', lastName: 'Nelson', username: 'Longstride', schoolname: 'Maple Grove Senior High', license: 'False'},
-    {firstName: 'Number', lastName: 'Two', username: 'Panda', schoolname: 'Maple Grove Senior High', license: 'False'}
+    { firstName: 'Peter', lastName: 'Nelson', username: 'Longstride', schoolname: 'Maple Grove Senior High', license: 'False' },
+    { firstName: 'And', lastName: 'Three', username: 'Scrafty', schoolname: 'Maple Grove Senior High', license: 'False' },
+    { firstName: 'Then', lastName: 'Alana', username: 'Ferroseed', schoolname: 'Maple Grove Senior High', license: 'False' },
+    { firstName: 'After', lastName: 'Mundi', username: 'Articuno', schoolname: 'Maple Grove Senior High', license: 'False' },
+    { firstName: 'Number', lastName: 'Two', username: 'Panda', schoolname: 'Maple Grove Senior High', license: 'False' }
   ])
 
   const [columnDefs] = useState([
-    { field: 'firstName'},
-    { field: 'lastName'},
-    { field: 'username'},
-    { field: 'schoolname'},
-    { field: 'license'},
+    { field: 'firstName', checkboxSelection: true },
+    { field: 'lastName' },
+    { field: 'username' },
+    { field: 'schoolname' },
+    { field: 'license' },
   ])
 
+  const onSuppressKeyboardEvent = params => {
+    const isBackspaceOrDeleteKey = (params.event.keyCode === 8) || (params.event.keyCode === 46)
+    if (isBackspaceOrDeleteKey) {
+      const selectedRows = params.api.getSelectedRows();
+      params.api.applyTransaction({ remove: selectedRows });
+      return true;
+    }
+  }
+
   return (
-    <div className="ag-theme-alpine" style={{ height: '768px', width: '1024px'}}>
+    <div className="ag-theme-alpine" style={{ height: '768px', width: '1024px' }}>
       <AgGridReact
+        rowSelection={"multiple"}
         rowData={rowData}
-        columnDefs={columnDefs}>
+        columnDefs={columnDefs}
+        defaultColDef={{
+          suppressKeyboardEvent: onSuppressKeyboardEvent
+        }}>
       </AgGridReact>
     </div>
   )
