9352b7bcbcb767e4a8c638a17571ce5908abdacc
Peter Nelson <afrusnoped@hotmail.com>
2022-04-29 13:27:49 -0500

    feat: add mysql, stub express method. add mysql table create script

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,27 @@
 const express = require('express')
-
 const PORT = process.env.PORT || 3001
-
 const app = express()
 
-app.get('/api', (req, res) => {
-  res.json({ message: 'Hello from server'})
+app.get('/student-list', (req, res) => {
+  console.log('i tried to get the mysql')
+  const mysql = require('mysql')
+  // port was 3307 i think
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'very_secure',
+    database: 'student_records'
+  })
+
+  connection.connect()
+
+  connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+    if (err) throw err
+
+    console.log(`The solution is: ${rows[0].solution}`)
+  })
+
+  connection.end()
 })
 
 app.listen(PORT, () => {
